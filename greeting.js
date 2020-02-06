29018e3b63ab9f38c8a9b068cb4674d25cb141db
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-02-06 08:51:59 -0800

    fixed condition in js file

--- greeting.js
+++ greeting.js
@@ -3,22 +3,22 @@ var today = new Date();
 var dayNow = today.getDay();
 var weekday;
 
-if (dayNow = 0) {
-    weekday = 'Sunday!';
-} else if (dayNow = 1) {
-    weekday = 'Monday!';
-}  else if (dayNow = 2) {
-    weekday = 'Tuesday!'; 
-}  else if (dayNow = 3) {
-    weekday = 'Wednesday!';  
-}  else if (dayNow = 4) {
-    weekday = 'Thursday!'; 
-}  else if (dayNow = 5) {
-    weekday = 'Friday!'; 
-}  else if (dayNow = 6) {
-    weekday = 'Saturday!'; 
+if (dayNow === 0) {
+    weekday = 'Sunday';
+} else if (dayNow === 1) {
+    weekday = 'Monday';
+}  else if (dayNow === 2) {
+    weekday = 'Tuesday'; 
+}  else if (dayNow === 3) {
+    weekday = 'Wednesday';  
+}  else if (dayNow === 4) {
+    weekday = 'Thursday'; 
+}  else if (dayNow === 5) {
+    weekday = 'Friday'; 
+}  else if (dayNow === 6) {
+    weekday = 'Saturday'; 
 } else {
-    weekday = 'a great day for hiking!';
+    weekday = 'Today';
 }
 
-document.write('<h3> Today is ' + weekday + '</h3>');
+document.write('<h3>' + weekday + ' is a great day for hiking!</h3>');
